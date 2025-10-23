e80ba5b4837af3b66f5790227319fbdab12c06b5
muhlatif1809 <muhlatif1809@gmail.com>
2025-10-23 20:29:43 +0700

    P9_Percobaan1

--- P9/SiakadFor23.java
+++ P9/SiakadFor23.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class SiakadFor23 {
     public static void main(String[] args) {
         Scanner latif23 = new Scanner(System.in);
-        double nilai23, tertinggi23 = 0, terendah23 = 100; 
-        int tidaLulus23 = 0, lulus23 = 0;
+        double nilai23, tertinggi23 = 0, terendah23 = 100;
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai Mahasiswa ke-" + i + ":");
             nilai23 = latif23.nextDouble();
@@ -14,19 +13,13 @@ public class SiakadFor23 {
                 tertinggi23 = nilai23;
             }
             if (nilai23 < terendah23) {
-                terendah23 = nilai23;
-            }
-            if (nilai23 >= 60) {
-                lulus23++;
-            } else {
-                tidaLulus23++;
+                terendah23 = terendah23;
+                
             }
             
         }
             System.out.println("Nilai Tertinggi: "  + tertinggi23);
             System.out.println("NIlai Terendah: "   + terendah23 );
-            System.out.println("Jumlah Mahasiswa Lulus Yaitu " + lulus23);
-            System.out.println("Jumlah Mahasiswa Tidak Lulus Yaitu" + tidaLulus23);
     }
     
 }
