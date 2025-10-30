9889c04291bbf1e8dbef19b01467a3e4d3f70088
muhlatif1809 <muhlatif1809@gmail.com>
2025-10-30 09:59:56 +0700

    P10_Percobaan 4

---
+++ P10/NilaiKelompok23.java
@@ -0,0 +1,28 @@
+package P10;
+
+import java.util.Scanner;
+
+public class NilaiKelompok23 {
+    public static void main(String[] args) {
+        Scanner latif23 = new Scanner(System.in);
+
+        int i = 1, nilai;
+        double rataNIlai;
+
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);   
+        
+        int totalNilai = 0;
+        for (int j = 1; j <= 5 ; j++) {
+            System.out.print("  Nilai Dari Kelompok Penilai " + j + ":");
+            nilai = latif23.nextInt();
+            totalNilai += nilai;  
+        }
+            rataNIlai = totalNilai / 5.0;
+            System.out.println("Kelompok : "    + i +   "Nilai Rata - Rata = " + rataNIlai );
+            i++;
+    }
+
+    }
+    
+}
