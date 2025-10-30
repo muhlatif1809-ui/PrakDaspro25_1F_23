d9c01219fa25eac27171f73d16a558beec2919c7
muhlatif1809 <muhlatif1809@gmail.com>
2025-10-30 09:10:45 +0700

    P10_Perconaan 1

---
+++ P10/Star23.java
@@ -0,0 +1,18 @@
+package P10;
+
+import java.util.Scanner;
+
+public class Star23 {
+    public static void main(String[] args) {
+        Scanner latif23 = new Scanner(System.in);
+
+        System.out.print("Masukkan Nilai n =");
+        int n = latif23.nextInt();
+        
+        for (int i = 1; i <=n; i++) {
+            System.out.print("*");
+            
+        }
+    }
+    
+}
