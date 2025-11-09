33f3fb1c4124dcadbb2291cf52bd7d8ab9c7b878
muhlatif1809 <muhlatif1809@gmail.com>
2025-11-09 19:02:06 +0700

    P11_Percobaan1_Pertanyaan1

--- P11/ArrayBilangan23.java
+++ P11/ArrayBilangan23.java
@@ -4,10 +4,10 @@ public class ArrayBilangan23 {
     public static void main(String[] args) {
         int [] bill = new int[4];
         
-        bill [0] = 5;
-        bill [1] = 13;
-        bill [2] = -7;
-        bill [3] = 17;
+        bill [0] = 5.0;
+        bill [1] = 1287;
+        bill [2] = 7.5;
+        bill [3] = 2000000;
 
         System.out.println(bill[0]);
         System.out.println(bill[1]);
