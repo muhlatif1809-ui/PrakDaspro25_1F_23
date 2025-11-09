b8ae2ef5b7cdcad62899a09b2a3690a2e271f3d8
muhlatif1809 <muhlatif1809@gmail.com>
2025-11-09 19:39:53 +0700

    p11_Percobaan3_Pertanyaan1

--- P11/ArrayRataNilai23.java
+++ P11/ArrayRataNilai23.java
@@ -6,6 +6,7 @@ public class ArrayRataNilai23 {
     public static void main(String[] args) {
         Scanner latif23 = new Scanner(System.in);
         int [] nilaiMhs =  new int[10];
+        int mhsLulus = 0;
         double total = 0;
         double rata2;
         
@@ -15,8 +16,13 @@ public class ArrayRataNilai23 {
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs [i] > 70) {
+                mhsLulus++;
+                
+            }
         }
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata Rata Nilai " + rata2);
+        System.out.println("Banyak Mahasiswa Lulus Yaitu : "+ mhsLulus + " Orang" );
     }
 }
