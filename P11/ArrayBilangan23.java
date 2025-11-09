378fd17e67e3fe2733120714fd8d24d66be8ecc0
muhlatif1809 <muhlatif1809@gmail.com>
2025-11-09 19:10:57 +0700

    P11_Percobaan1_Pertanyaan4

--- P11/ArrayBilangan23.java
+++ P11/ArrayBilangan23.java
@@ -3,7 +3,7 @@ package P11;
 public class ArrayBilangan23 {
     public static void main(String[] args) {
         int [] bill = {5,13,-7,17};
-        for (int i = 0; i < 4 ; i++) {
+        for (int i = 0; i <= 4 ; i++) {
             System.out.println(bill[i]);
         }
 
