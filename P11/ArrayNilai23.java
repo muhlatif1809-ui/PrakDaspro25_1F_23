67de644f1be0a8c4b3e738dfdff884b603ae2b4f
muhlatif1809 <muhlatif1809@gmail.com>
2025-11-09 19:32:03 +0700

    P11_Percobaan2_Pertanyaan4

--- P11/ArrayNilai23.java
+++ P11/ArrayNilai23.java
@@ -14,6 +14,10 @@ public class ArrayNilai23 {
         for (int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] > 70) {
                 System.out.println("Mahasiswa ke - " + i + " Lulus");
+            if (nilaiAkhir[i] <= 70) {
+                System.out.println("Mahasiswa ke - " + i + " Tidak Lulus");
+                    
+                }
                 
             }
             //System.out.println("Nilai Akhir Ke- " + i + " Adalah " + nilaiAkhir[i]);
