318b7b13c5aa55e4c03e4925862d06ed81f2fba4
muhlatif1809 <muhlatif1809@gmail.com>
2025-11-09 20:50:50 +0700

    P11_Percobaan4_Pertanyaan2

--- P11/SearchNilai23.java
+++ P11/SearchNilai23.java
@@ -1,21 +1,31 @@
 package P11;
 
+import java.util.Scanner;
+
 public class SearchNilai23 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner latif23 = new Scanner(System.in);
+        System.out.print("Masukkan Nilai Yang Akan Di Input : ");
+        int nilai = latif23.nextInt();
+        int[] arrNilai = new int[nilai];
+        int key = 0, hasil = 0;
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan Nilai Mahasiswa Ke - " +(i+1)+ " : ");
+            arrNilai [i] = latif23.nextInt();    
+        }
+        System.out.print("Masukkan Nilai Yang Ingin Dicari : ");
+        key = latif23.nextInt();
 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
-                break;
-                
+                hasil = i + 1;
+                break;  
             }
             
         }
             System.out.println();
-            System.out.println("Nilai "+key+ " Ketemu Di Indeks Ke-" + hasil);
+            System.out.println("Nilai "+key+ " Ketemu, Merupakan Nilai  Mahasiswa Ke-" + hasil);
             System.out.println();
     }
     
