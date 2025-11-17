3969840efae263ac756115c2d6ac090ee8439345
muhlatif1809 <muhlatif1809@gmail.com>
2025-11-17 17:18:08 +0700

    P12_Percobaan1_Pertanyaan5

--- P12/Bioskop23.java
+++ P12/Bioskop23.java
@@ -13,15 +13,19 @@ public class Bioskop23 {
         penonton [3][0] = "Gisel";
         penonton [3][1] = "Hana"; 
 
+        
+        System.out.println(penonton.length);
+        
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang Baris Ke  - " + (i+1) + ":" + penonton[i].length);
+        }
+
+
         System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
 
-        System.out.println(penonton.length);
-        System.out.println(penonton[0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton[2].length);
-        System.out.println(penonton[3].length);
+       
     }
 }
